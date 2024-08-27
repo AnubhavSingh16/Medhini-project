c68c9f80b0fa2e8010d708b601a55a2fff6c0d7b
AnubhavSingh16 <anubhavsrajput54@gmail.com>
2024-08-27 13:24:41 +0530

    profile bg

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,20 +2,18 @@ import React from "react";
 
 function Profile() {
   return (
-    <>
-      <div className="relative bg-cover bg-center h-screen">
-        <img
-          src="/forest.jpg"
-          alt="bg"
-          className="w-full h-full object-cover "
-        />
-        <div className="absolute inset-0 flex justify-center items-center">
-          <h2 className="text-white text-lg  font-mono text-center">
-            Most of us experience mental and physical imbalance
-          </h2>
-        </div>
+    <div className="relative bg-cover bg-center h-screen">
+      <img
+        src="/forest.jpg"
+        alt="bg"
+        className="w-full h-full object-cover mix-blend-plus-darker"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-40 flex justify-center items-center">
+        <h2 className="text-white text-lg font-thin-semibold text-center px-4 py-2">
+          Most of us experience mental and physical imbalance
+        </h2>
       </div>
-    </>
+    </div>
   );
 }
 
